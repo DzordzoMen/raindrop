e1aec7249ab88d0a5a7b93d32afac287595a301b
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-11-06 16:51:19 +0100

    no message

---
+++ web/raindrop-web/src/components/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <the-header />
+    <the-navigation-drawer />
+  </v-app>
+</template>
+
+<script>
+import TheHeader from '@/components/TheHeader.vue';
+import TheNavigationDrawer from '@/components/TheNavigationDrawer.vue';
+
+export default {
+  name: 'Dashboard',
+  components: {
+    TheHeader,
+    TheNavigationDrawer,
+  },
+};
+</script>
--- web/raindrop-web/src/components/TheNavigationDrawer.vue
+++ web/raindrop-web/src/components/TheNavigationDrawer.vue
@@ -1,24 +1,10 @@
 <template>
-    <div id="app">
-  <v-app id="inspire">
-    <v-card
-      height="400"
-      width="256"
-      class="mx-auto"
-    >
-      <v-navigation-drawer permanent>
-        <v-list-item>
-          <v-list-item-content>
-            <v-list-item-title class="text-h6">
-              Application
-            </v-list-item-title>
-            <v-list-item-subtitle>
-              subtext
-            </v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-divider></v-divider>
+    <v-app id="inspire">
+      <v-navigation-drawer v-model="drawer"
+      permanent
+      left
+      absolute
+      class="v-theme--dark">
         <v-list
           dense
           nav
@@ -38,7 +24,24 @@
           </v-list-item>
         </v-list>
       </v-navigation-drawer>
-    </v-card>
   </v-app>
-</div>
 </template>
+
+<script>
+
+export default {
+  name: 'TheNavigationDrawer',
+  data() {
+    return {
+      items: [
+        { title: 'O nas', icon: 'mdi-view-dashboard' },
+        { title: 'Jak zacząć?', icon: 'mdi-image' },
+        { title: 'Dotacje', icon: 'mdi-image' },
+        { title: 'Ile możesz zaoszczędzić?', icon: 'mdi-image' },
+        { title: 'Jak dużo wody zużywasz?', icon: 'mdi-image' },
+      ],
+      right: null,
+    };
+  },
+};
+</script>
