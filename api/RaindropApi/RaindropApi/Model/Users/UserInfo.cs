9fb8ccca908779576b85ee298442327b818c68f0
Morasiu <moras2601@wp.pl>
2021-11-06 18:47:00 +0100

    User Info put

---
+++ api/RaindropApi/RaindropApi/Model/Users/UserInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace RaindropApi.Model.Users {
+	public class UserInfo {
+		public int UserId { get; set; }
+		public decimal TotalCollectedAmount { get; set; }
+
+		public decimal RoofArea { get; set; }
+	}
+}
--- api/RaindropApi/RaindropApi/Model/Users/UserStat.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace RaindropApi.Controllers {
-	public class UserStat {
-		public int UserId { get; set; }
-		public decimal TotalCollectedAmount { get; set; }
-	}
-}
