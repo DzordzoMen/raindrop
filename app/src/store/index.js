0f33728cba91b96a2346f2516c5816d924367dc3
Maciej Romanowski <neipreis22@gmail.com>
2021-11-06 21:32:05 +0100

    added vuex with state for devices and weather

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,15 +1,37 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import Api from '../axios/api';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    devices: [],
+    weather: [],
+  },
+  getters: {
+    firstDevice: (state) => state.devices[0] || {},
+    allWeather: (state) => state.weather || [],
+    currentWeather: (state) => state.weather[0] || {},
   },
   mutations: {
+    setDevices(state, data) {
+      state.devices = data;
+    },
+    setWeather(state, data) {
+      state.weather = data;
+    },
   },
   actions: {
-  },
-  modules: {
+    fetchDevices({ commit }) {
+      Api.get('/tanks').then(({ data }) => {
+        commit('setDevices', data);
+      });
+    },
+    fetchWeather({ commit }) {
+      Api.get('/weatherForecast').then(({ data }) => {
+        commit('setWeather', data);
+      });
+    },
   },
 });
