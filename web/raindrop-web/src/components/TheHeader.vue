6d6bc94c67358f66cb4d78fe1672605c6eb7001f
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-11-06 14:57:37 +0100

    no message

---
+++ web/raindrop-web/src/components/TheHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app-bar
+    app
+    color="primary lighten-2"
+    dark
+  >
+    <div class="d-flex align-center" style="gap: 10px">
+      <v-img
+        alt="Raindrop Logo"
+        contain
+        src="../assets/logo.svg"
+        width="40"
+        height="40"
+      />
+      <span class="headline">
+        Raindrop
+      </span>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+};
+</script>
