b6f8b39b0e1d4682fb0d041ba3070dcecead7da7
Maciej Romanowski <neipreis22@gmail.com>
2021-11-07 03:16:29 +0100

    added dates in weather view

--- app/src/views/Weather.vue
+++ app/src/views/Weather.vue
@@ -3,6 +3,7 @@
     <v-row dense>
       <v-col cols="12" v-for="(weather, index) in weatherForWeek" :key="index">
         <weather-card
+          :date="dates[index]"
           :icon="weather.weather[0].icon"
           :name="weather.weather[0].main"
           :temp="weather.dayTemperature"
@@ -24,6 +25,15 @@ export default {
   computed: {
     weatherForWeek() {
       return this.$store.getters.allWeather || [];
+    },
+    dates() {
+      const array = [];
+      for (let i = 0; i < this.weatherForWeek.length; i += 1) {
+        const date = new Date();
+        date.setDate(date.getDate() + i);
+        array.push(date);
+      }
+      return array;
     },
   },
 };
