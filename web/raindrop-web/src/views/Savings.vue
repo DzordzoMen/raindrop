1c0f1aadc1a101869e6c996a2da3088b73a66ac0
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-11-07 03:05:02 +0100

    no message

--- web/raindrop-web/src/views/Savings.vue
+++ web/raindrop-web/src/views/Savings.vue
@@ -1,5 +1,11 @@
 <template>
     <v-container>
-        Savings
+        <div>
+            <h2>
+                Oblicz swój potencjał oszczędzania wody
+            </h2>
+        </div>
+        <div>
+        </div>
     </v-container>
 </template>
